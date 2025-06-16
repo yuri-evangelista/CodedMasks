1e1549e103e98550cb445bab81628efee5aa89d2
yuri-evangelista <82113157+yuri-evangelista@users.noreply.github.com>
2025-06-16 16:57:54 +0200

    First commit, dirty code...

---
+++ mask_utils.py
@@ -0,0 +1,292 @@
+import numpy as np
+from sympy import *
+import numpy as np
+import astropy.io.fits as pyfits 
+from astropy.table import Table, Column
+
+
+def ura_mura(p):
+    #Checking if p is prime
+    if isprime(p):
+        #Preparing arrays
+        A = np.zeros(p, dtype=int)
+        R = np.arange(p, dtype=np.int64)**2 % p
+        A[R] = 1
+
+        #Check if URA or MURA can be generated
+        URA = ( round( (p-3)/4 ) - ((p-3)/4)) == 0
+        MURA = ( round( (p-1)/4 ) - ((p-1)/4)) == 0
+
+        if URA:
+            print("Generating URA array")
+        elif MURA:
+            print("Generating MURA array")
+            A[0] = 0
+        return A
+    else:
+        raise TypeError("p must be prime")
+
+def bura(p, modified=False):
+    #Checking if p is prime
+    if not isprime(p):
+        raise TypeError("Number of array elements must be prime")
+
+    #Check if BURA can be generated
+    x = np.sqrt( (p-1)/4)
+    if not ( (int(x) == x) & ( (x % 2) == 1) ) :
+        raise TypeError("p does not fulfill the requirement of p = 4x^2 + 1 with x odd")
+        
+    #Preparing arrays
+    A = np.zeros(p, dtype=np.int64)
+    R = np.arange(p, dtype=np.int64)**4 % p
+    A[R] = 1
+
+    if modified:
+        A[0] = 0
+        print("Generating M-BURA array")
+    else:
+        print("Generating BURA array")
+    
+    return A
+
+def shift(arr, lag):
+    shifted = np.roll(arr, lag, axis=1)
+    if lag >=0:
+        shifted[:, 0:lag] = 0
+    else:
+        shifted[:, lag:] = 0
+    return shifted
+
+def erosion(arr, cut, step):
+    r"""
+    Function to erode mask array for vignetting.
+    2D matrix erosion for simulating finite thickness effect in shadow projections.
+    
+    It takes a mask array and "thins" the mask elements across the columns' direction.
+    The erosion is performed only on the correct side of open (1) mask elements:
+        - right side if cut is negative (= thetaX negative)
+        - left side if cut is positive (= thetaX positive)
+    The function first erodes all integer bins (replacing 1s with 0s)
+    If cut is not integer, then the function applies a fractional transparency to the last eroded bin
+
+              \\       \\  \\
+    ___________\\       \\  \\____________
+               |\\       \\ |\
+    ___________| \\       \\|_\___________
+                  \\       \\  \ 
+                   \\       \\  \
+    ________________\\_______\\__\_________
+               <--->           <->
+               SHIFT         EROSION   
+    
+    """
+    ncuts = int(cut / step)  #find the integer numbers of bins to cut
+    decimal = abs(cut/step - ncuts) #find the decimal number of bins to cut
+    
+    #Find arr indexes to be cut (completely)
+    shifted = shift(arr, ncuts)
+    if ncuts:
+        eroded_int = arr * ( (arr-shifted) > 0)
+    else:
+        eroded_int = arr * 0
+    
+    #Finds arr indexes to be fractionally reduced
+    if decimal:
+        shifted_p1 = shift(shifted, int(np.sign(cut)))
+        eroded_frac = arr * (( (shifted_p1 - arr) < 0) & ~eroded_int)
+    else:
+        eroded_frac = arr * 0
+
+
+    #Calculate output array
+    out = (arr * (eroded_int < 1)) - eroded_frac * decimal
+
+    return out
+
+def next_prime(n):
+    p=n
+    while (not isprime(p)):
+        p+=1
+    return(p)
+
+def get_openfraction(mask):
+	return np.sum(mask)/mask.size
+
+def get_angular_res(m_pitch, d_pitch, m_d_distance, degrees=False):
+	res = np.sqrt( np.arctan(m_pitch/m_d_distance)**2 + np.arctan(d_pitch/m_d_distance)**2)
+	if degrees:
+		res = np.rad2deg(res)
+	return res
+
+def get_coding_power(m_pitch, d_pitch, open_fraction):
+	#from Skinner 2008
+	delta = (1 - d_pitch/(3*m_pitch) * np.sqrt(4*open_fraction*(1-open_fraction)))
+	return delta
+
+def open_fraction_vs_off_axis(mask, mask_thickness, mask_x_pitch, mask_y_pitch, thetaX, thetaY, degrees=True):
+	if degrees:
+		thetaX = np.deg2rad(thetaX)
+		thetaY = np.deg2rad(thetaY)
+	cutx = mask_thickness * np.tan(thetaX) / mask_x_pitch
+	cuty = mask_thickness * np.tan(thetaY) / mask_y_pitch
+	#print(cutx, cuty)
+	vignetted_x = erosion(mask, cutx, 1)
+	vignetted_y = erosion(mask.T, cuty, 1)
+	#print(get_openfraction(vignetted_x))
+	#print(get_openfraction(vignetted_y))
+	return get_openfraction(vignetted_x * vignetted_y.T)
+
+def pad_array(arr, npadx, npady):
+	return np.pad(arr, ( (npadx, npadx), (npady, npady)), 'constant', constant_values=(0,0))
+
+def float_gcd(a, b, rtol = 1e-5, atol = 1e-5):
+	#Greatest common divisor for floating point numbers (with tolerances)
+    t = min(abs(a), abs(b))
+    while abs(b) > rtol * t + atol:
+        a, b = b, a % b
+    return round(a/atol)*atol
+
+def upscale(array, fx, fy):
+    for ax, factor in enumerate((fy, fx)):
+        array = np.repeat(array, factor, axis=ax)
+    return array
+
+def eff_area_vs_off_axis(mask, det, x_pitch_ups, y_pitch_ups, focal, mask_thickness, thetaX, thetaY, degrees=True):
+    # Returns the area in same units of the inputs (mm2 if pitch and focal are mm, cm2 if pitch and focal are cm)
+    # It does (!) take into account of vignetting 
+    if degrees:
+        thetaX = np.deg2rad(thetaX)
+        thetaY = np.deg2rad(thetaY)
+
+    #Apply vignetting
+    cutx = mask_thickness * np.tan(thetaX) / x_pitch_ups
+    cuty = mask_thickness * np.tan(thetaY) / y_pitch_ups
+    vignetted_x = erosion(mask, cutx, 1)
+    vignetted_y = erosion(mask.T, cuty, 1)
+    vignetted = vignetted_x * vignetted_y.T
+
+    x_shift_px, y_shift_px = int(focal * np.tan(thetaX)/x_pitch_ups), int(focal * np.tan(thetaY)/y_pitch_ups)
+    #print(x_shift_px, y_shift_px)
+    m = shift(vignetted, x_shift_px) #CHECK
+    m = shift(m.T, y_shift_px) #CHECK
+    m = m.T
+    #print(get_openfraction(m))
+    eff_area = np.sum( m * (  det > 0) ) * x_pitch_ups * y_pitch_ups
+
+    return eff_area
+
+
+def read_mask_bulk(fitsfile, ext):
+    r"""
+    Reads data from wfm_mask.fits
+    Extensions are:
+        0  PRIMARY       1 PrimaryHDU      28   ()      
+        1  OR_MASK       1 BinTableHDU     36   676000R x 3C   [E, E, E]   
+        2  MASK          1 BinTableHDU     36   676000R x 3C   [E, E, E]   
+        3  RMATRIX       1 BinTableHDU     38   676000R x 3C   [E, E, E]   
+        4  SENS          1 BinTableHDU     36   676000R x 3C   [E, E, E]   
+    """
+
+    with pyfits.open(fitsfile) as hdu_list:     
+        hdu_list.info()
+        header = hdu_list[ext].header
+        NELE   = header['NAXIS2']
+        ELXDIM = header['ELXDIM']
+        ELYDIM = header['ELYDIM']
+        ELXN   = header['ELXN']
+        ELYN   = header['ELYN']
+        MINX   = header['MINX']
+        MINY   = header['MINY']
+    
+    
+        data=Table(hdu_list[ext].data)
+        arr = np.zeros((ELXN, ELYN))
+        for ele in range(NELE):
+            ix = int(np.round(  ((data['X'][ele] - MINX - ELXDIM/2)/ELXDIM) ) )
+            iy = int(np.round(  ((data['Y'][ele] - MINY - ELYDIM/2)/ELYDIM) ) )
+            arr[ix,iy] = data['VAL'][ele]
+
+        return arr
+
+
+
+def omega_plate_offaxis(a, b, d, A, B):
+    r"""
+    https://vixra.org/pdf/2001.0603v2.pdf eq.34
+    This is when the line-of-sight hits the rectangular plate off center
+    - d is the distance between the plate and the "observer"
+    - a, b are the dimensions of the rectangular plate
+    - A, B are the corner distance from the observer (0<= A <=a/2, 0<= B <=b/2)
+    That means we have 4 rectangles (with the observer located on one corner of each one):
+        r1 = A * (b-B)
+        r2 = (a-A) * (b-B)
+        r3 = A * B
+        r4 = (a-A) * B
+
+    <-----------a----------->
+
+     _______|_______________    
+    |       |               |   |
+    |   1   |       2       |   |
+    |       |               |   |
+    |___A___|_______________|   |b
+    |       |               |   |
+    |   3  B|       4       |   |    
+    |_______|_______________|   |
+            |
+    
+    """
+
+    def omega_plate(a, b, d):
+        r"""
+        https://vixra.org/pdf/2001.0603v2.pdf eq.27
+        a, b are the dimensions of the rectangular plate
+        d is the distance of the "observer" from the plate
+        """
+        alpha = a/(2*d)
+        beta  = b/(2*d)
+        return 4 * np.arctan( (alpha * beta)/ np.sqrt(1 + alpha**2 + beta**2) )
+
+    omega1 = omega_plate(2*A,2*(b-B), d)
+    omega2 = omega_plate( 2*(a-A), 2*(b-B), d)
+    omega3 = omega_plate(2*A, 2*B, d)
+    omega4 = omega_plate(2*(a-A), 2*B, d)
+
+    return (omega1 + omega2 + omega3 + omega4)/4
+
+
+
+def solid_angle(bulk, xstep, ystep, m_d_distance, nobulk=False):
+    """
+    bulk_shape = shape(bulk)
+    xsize, ysize = (bulk_shape[0] * xstep, bulk_shape[1] * ystep)
+    xcoords = np.arange(0, bulk_shape[0]) * xstep + xstep/2 - xsize/2
+    ycoords = np.arange(0, bulk_shape[1]) * ystep + ystep/2 - ysize/2
+
+    omega = np.zeros(bulk_shape)
+
+    for ix in range(bulk_shape[0]):
+        for iy in range(bulk_shape[1]):
+            A = xsize/2 - abs(xcoords[ix])
+            B = ysize/2 - abs(ycoords[iy])
+            omega[ix, iy] = omega_plate_offaxis(xsize, ysize, m_d_distance, A, B) * bulk[ix, iy]
+
+    return omega
+    """
+    bulk = np.asarray(bulk)
+    xsize, ysize = bulk.shape[0] * xstep, bulk.shape[1] * ystep
+
+    #Generates arrays of pixel centers
+    xcoords = (np.arange(bulk.shape[0]) + 0.5) * xstep - xsize/2
+    ycoords = (np.arange(bulk.shape[1]) + 0.5) * ystep - ysize/2
+
+    #Calculates A, B for each pixel
+    A = xsize / 2 - np.abs(xcoords[:, np.newaxis]) #shape: (Nx, 1)
+    B = ysize / 2 - np.abs(ycoords[np.newaxis, :]) #shape: (1, Ny)
+
+    if not nobulk:
+        #Calculates omega for each pixel using bulk as a bitmask
+        omega_values = omega_plate_offaxis(xsize, ysize, m_d_distance, A, B) * (bulk > 0)
+    else:
+        omega_values = omega_plate_offaxis(xsize, ysize, m_d_distance, A, B)
+    return omega_values
